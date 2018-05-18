1a6e1f47430e5761c7d3b70b934196a200ce8734
vijithavijayakumar <37609638+vijithavijayakumar@users.noreply.github.com>
2018-05-18 11:35:32 +0530

    Add files via upload

---
+++ constants.go
@@ -0,0 +1,12 @@
+package main
+ 
+import "fmt"
+import "math"
+ const s string = "constant"
+ func main(){
+ fmt.Println(s)
+ const n = 100000
+ const d = 5/n
+ fmt.Println(d)
+fmt.Println(math.Sin(n))
+}
